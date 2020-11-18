f07bb23699e25619de81083b58a1fe4af79a460f
Elias Farhan <e.farhan@sae.edu>
2020-11-18 10:37:27 +0100

    Adding player character, horizontal and animations

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public enum State
+    {
+        None,
+        Idle,
+        Walk,
+        Jump
+    }
+
+    private State currentState_ = State.None;
+
+    [SerializeField] private Animator anim_;
+
+    private const float deadZone_ = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeState(State.Idle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move += 1.0f;
+        }
+        switch (currentState_)
+        {
+            case State.Idle:
+                if (Mathf.Abs(move) > deadZone_)
+                {
+                    ChangeState(State.Walk);
+                }
+                break;
+            case State.Walk:
+                if (Mathf.Abs(move) < deadZone_)
+                {
+                    ChangeState(State.Idle);
+                }
+                break;
+            case State.Jump:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    void ChangeState(State state)
+    {
+        switch (state)
+        {
+            case State.Idle:
+                anim_.Play("Idle");
+                break;
+            case State.Walk:
+                anim_.Play("Walk");
+                break;
+            case State.Jump:
+                anim_.Play("Jump");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(state), state, null);
+        }
+
+        currentState_ = state;
+    }
+}
